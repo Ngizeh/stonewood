b4b71a1878d20c2b246886f475cd6c3d57f17169
Ngizeh <edngize@gmail.com>
2017-12-08 16:28:14 +0300

    links

--- resources/views/layouts/partial.blade.php
+++ resources/views/layouts/partial.blade.php
@@ -1,19 +1,22 @@
 @foreach($property->all() as $value)
-
     <div class="row index">
-        <div class="col-md-4">
-            @foreach($value->propertyPhotos as $set)
-                @if($loop->first)
-                    <img src="/{{$set->thumbnail_path }}" alt="{{$set->name}}">
-                @endif
-            @endforeach
-        </div>
-        <div class="col-md-8">
-            <h2>{{$value->title}}</h2>
-            <p class="big"><strong> Ksh. {{number_format($value->price)}} at {{$value->location}}</strong></p>
-            <p>{{str_limit($value->description, 300, ' ...')}} <a href="{{url('property/'.$value->title)}}"> &lsqb; Read More &rsqb;</a></p>
-        </div>
+        <a href="{{'property/'. $value->title}}">
+            <div class="col-md-4">
+                @foreach($value->propertyPhotos as $set)
+                    @if($loop->first)
+                        <img src="/{{$set->thumbnail_path }}" alt="{{$set->name}}">
+                    @endif
+                @endforeach
+            </div>
+        </a>
+            <div class="col-md-8">
+                <h2>{{$value->title}}</h2>
+                <p class="big"><strong> Ksh. {{number_format($value->price)}} at {{$value->location}}</strong></p>
+                <p>{{str_limit($value->description, 300, ' ...')}} <a href="{{url('property/'.$value->title)}}"> &lsqb;
+                        Read More &rsqb;</a></p>
+            </div>
     </div>
+
     <hr>
 @endforeach
 {{ $property->links() }}
