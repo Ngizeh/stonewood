e7ac18bdffb96294013ea3845cf9a28ce90dab9c
Ngizeh <edngize@gmail.com>
2018-01-23 12:32:02 +0300

    few edits on front end

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,52 +1,85 @@
+<div class="footer">
 
-<div class="footer-distributed" id="contact">
+    <div class="container">
 
-    <div class="footer-left">
 
-        <h3>HomeAssured</h3>
+        <div class="row">
+            <div class="col-lg-3 col-sm-3">
+                <h4>Information</h4>
+                <ul class="row">
+                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="about.php">About</a></li>
+                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="agents.php">Agents</a></li>
+                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="blog.php">Blog</a></li>
+                    <li class="col-lg-12 col-sm-12 col-xs-3"><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
 
-        <p class="footer-links">
-            <a href="{{url('/')}}">Home</a>
-            ·
-            <a href="{{url('about')}}">About</a>
-            ·
-            <a href="{{url('/property')}}">Property</a>
-            ·
-            <a href="#contact">Contacts Us</a>
-            .
-        </p>
+            <div class="col-lg-3 col-sm-3">
+                <h4>Newsletter</h4>
+                <p>Get notified about the latest properties in our marketplace.</p>
+                <form class="form-inline" role="form">
+                    <input type="text" placeholder="Enter Your email address" class="form-control">
+                    <button class="btn btn-success" type="button">Notify Me!</button>
+                </form>
+            </div>
 
-        <p class="footer-company-name">HomeAssured &copy; 2008 </p>
-    </div>
+            <div class="col-lg-3 col-sm-3">
+                <h4>Follow us</h4>
+                <a href="#"><img src="images/facebook.png" alt="facebook"></a>
+                <a href="#"><img src="images/twitter.png" alt="twitter"></a>
+                <a href="#"><img src="images/linkedin.png" alt="linkedin"></a>
+                <a href="#"><img src="images/instagram.png" alt="instagram"></a>
+            </div>
 
-    <div class="footer-center">
-
-        <div>
-            <i class="fa fa-map-marker"></i>
-            <p>Jalaram Rd, Westlands</p>
+            <div class="col-lg-3 col-sm-3">
+                <h4>Contact us</h4>
+                <p><b>HomeAssured Property</b><br>
+                    <span class="glyphicon glyphicon-map-marker"></span> 8290 Walk Street, Australia <br>
+                    <span class="glyphicon glyphicon-envelope"></span> hello@bootstrapreal.com<br>
+                    <span class="glyphicon glyphicon-earphone"></span> (123) 456-7890</p>
+            </div>
         </div>
+        <p class="copyright">Copyright 2013. All rights reserved. </p>
 
-        <div>
-            <i class="fa fa-phone"></i>
-            <p>+254 20 207 7030</p>
-        </div>
-
-        <div>
-            <i class="fa fa-envelope"></i>
-            <p><a href="mailto:sales@homeassured.property">sales@homeassured.property</a></p>
-        </div>
-
-    </div>
-
-    <div class="footer-right">
-        <div class="footer-icons">
-
-            <a href="https://web.facebook.com/homeassuredkenya" target="_blank"><i class="fa fa-facebook"></i></a>
-            <a href="https://twitter.com/homeassured_ltd" target="_blank"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-github"></i></a>
-
-        </div>
 
     </div>
-
 </div>
+
+
+<!-- Modal -->
+<div id="loginpop" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="row">
+                <div class="col-sm-6 Rogin">
+                    <h4>Login</h4>
+                    <form class="" role="form">
+                        <div class="form-group">
+                            <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only" for="exampleInputPassword2">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword2"
+                                   placeholder="Password">
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Remember me
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-success">Sign in</button>
+                    </form>
+                </div>
+                <div class="col-sm-6">
+                    <h4>New User Sign Up</h4>
+                    <p>Join today and get updated with all the properties deal happening around.</p>
+                    <button type="submit" class="btn btn-info" onclick="window.location.href='register.php'">Join Now
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
