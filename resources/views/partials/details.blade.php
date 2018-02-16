b0f3514634ed45e5d6c32ab197bb9e222e2766dd
Ngizeh <edngize@gmail.com>
2018-02-16 12:07:17 +0300

    something on views

--- resources/views/partials/details.blade.php
+++ resources/views/partials/details.blade.php
@@ -1,13 +1,13 @@
 showsohbb<div class="container">
     <div class="properties-listing">
         <div class="row">
-            <div class="col-lg-3">
+            <div class="col-md-3">
                 @include('partials.search')
                 @if(Auth::check())
                 <a href="{{'/property/create'}}" class="btn btn-primary">Create A Property</a>
                 @endif
             </div>
-            <div class="col-lg-9 gallary">
+            <div class="col-md-9 gallary">
                 <hr>
                 @foreach($properties as $property)
                     <div class="col-md-4 col-sm-6 gallary__image">
