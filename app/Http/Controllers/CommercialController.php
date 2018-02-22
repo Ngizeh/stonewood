8548fe34fd340e9c5c9e44d35edcb0405436fe35
Ngizeh <edngize@gmail.com>
2018-02-22 18:04:28 +0300

    removed order by

--- app/Http/Controllers/CommercialController.php
+++ app/Http/Controllers/CommercialController.php
@@ -13,7 +13,7 @@ class CommercialController extends Controller
 {
   public function index()
   {
-      $properties = Property::whereNotNull('commercial')->latest()->paginate(9);
+      $properties = Property::whereNotNull('commercial')->paginate(9);
       
       return view('.commercial.commercials', compact('properties'));
   }
@@ -21,7 +21,7 @@ class CommercialController extends Controller
   public function sales()
   {
     $properties = Property::where('listed', '=', 'Sale')
-                ->whereNotNull('commercial')->latest()->paginate(9);
+                ->whereNotNull('commercial')->paginate(9);
 
     return view('commercial.sales', compact('properties'));
   }
@@ -29,7 +29,7 @@ class CommercialController extends Controller
   public function rentals()
   {
       $properties = Property::where('listed', '=', 'Rental')
-                  ->whereNotNull('commercial')->latest()->paginate(9);
+                  ->whereNotNull('commercial')->paginate(9);
 
       return view('commercial.rentals', compact('properties'));
   }
