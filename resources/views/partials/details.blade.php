ac275310fc098538a656590407e6f0846baa9f2a
Ngizeh <edngize@gmail.com>
2018-02-22 16:47:43 +0300

    arrange

--- resources/views/partials/details.blade.php
+++ resources/views/partials/details.blade.php
@@ -9,22 +9,23 @@
             <div class="col-md-9">
                 <hr>
                 <div class="row">
-                @foreach($properties as $property)
-                    <div class="col-md-4 col-sm-6">
-                        <div class="properties">
-                        @foreach($property->propertyPhotos as $set)
-                                @if($loop->first)
+                    @foreach($properties as $property)
+                        <div class="col-md-4 col">
+                            <div class="properties">
+                                @foreach($property as $item)
+                                @foreach($property->propertyPhotos as $set)
+                                        @if($loop->first)
                                         <div class="image-holder">
-                                            <img src="/{{$set->thumbnail_path}}" class="img-responsive" alt="properties">
-                                        </div>
-                            @endif
-                            @endforeach
-                                <h4><a href="{{url('/property/'.$property->title_heading())}}" style="color: #ff770e"><strong>{{$property->title}}</strong></a></h4>
-                            <p class="price">@include('partials.currency') {{$property->price_format()}}</p>
-                            <a class="btn btn-primary" href="{{url('/property/'.$property->title_heading())}}">View Details</a>
+                                        <img src="/{{$set->thumbnail_path}}" class="img-responsive"
+                                    alt="properties">
+                                     </div>
+                                        @endif
+                                    @endforeach
+                                @endforeach
+                                <p>{{$property->title}}</p>
+                            </div>
                         </div>
-                          </div>
-                    @endforeach
+                        @endforeach
                 </div>
                 <div class="center">
                     {{ $properties->links() }}
@@ -33,3 +34,43 @@
         </div>
     </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ {{--{    @foreach($properties as $property)--}}
+         {{--<div class="col-md-4 col-sm-6">--}}
+             {{--@foreach($property as $item)--}}
+                 {{--<div class="properties">--}}
+                     {{--@foreach($property->propertyPhotos as $set)--}}
+                         {{--@if($loop->first)--}}
+                             {{--<div class="image-holder">--}}
+                                 {{--<img src="/{{$set->thumbnail_path}}" class="img-responsive"--}}
+                                      {{--alt="properties">--}}
+                             {{--</div>--}}
+                         {{--@endif--}}
+                     {{--@endforeach--}}
+                     {{--<h4><a href="{{url('/property/'.$property->title_heading())}}"--}}
+                            {{--style="color: #ff770e"><strong>{{$property->title}}</strong></a></h4>--}}
+                     {{--<p class="price">@include('partials.currency') {{$property->price_format()}}</p>--}}
+                     {{--<a class="btn btn-primary" href="{{url('/property/'.$property->title_heading())}}">View--}}
+                         {{--Details</a>--}}
+                 {{--</div>--}}
+             {{--@endforeach--}}
+         {{--</div>--}}
+ {{--</div>--}}
+ {{--@endforeach}--}}
