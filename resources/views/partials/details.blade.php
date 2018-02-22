df7b6b5a7b9dcff640838f6576a0c5a6eee76ebc
Ngizeh <edngize@gmail.com>
2018-02-22 17:36:48 +0300

    pagination is off

--- resources/views/partials/details.blade.php
+++ resources/views/partials/details.blade.php
@@ -30,50 +30,11 @@
                         @endforeach
                     </div>
                 @endforeach
+            </div>
                 <div class="center">
                     {{ $properties->links() }}
                 </div>
             </div>
         </div>
-    </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- {{--{    @foreach($properties as $property)--}}
-         {{--<div class="col-md-4 col-sm-6">--}}
-             {{--@foreach($property as $item)--}}
-                 {{--<div class="properties">--}}
-                     {{--@foreach($property->propertyPhotos as $set)--}}
-                         {{--@if($loop->first)--}}
-                             {{--<div class="image-holder">--}}
-                                 {{--<img src="/{{$set->thumbnail_path}}" class="img-responsive"--}}
-                                      {{--alt="properties">--}}
-                             {{--</div>--}}
-                         {{--@endif--}}
-                     {{--@endforeach--}}
-                     {{--<h4><a href="{{url('/property/'.$property->title_heading())}}"--}}
-                            {{--style="color: #ff770e"><strong>{{$property->title}}</strong></a></h4>--}}
-                     {{--<p class="price">@include('partials.currency') {{$property->price_format()}}</p>--}}
-                     {{--<a class="btn btn-primary" href="{{url('/property/'.$property->title_heading())}}">View--}}
-                         {{--Details</a>--}}
-                 {{--</div>--}}
-             {{--@endforeach--}}
-         {{--</div>--}}
- {{--</div>--}}
- {{--@endforeach}--}}
