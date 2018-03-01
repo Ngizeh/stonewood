0321550613089f5b466b615d1e8dd51b1bafc59c
Ngizeh <edngize@gmail.com>
2018-03-01 12:25:13 +0300

    commits

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -238,7 +238,7 @@
             var places = searchBox.getPlaces();
             var bounds = new google.maps.LatLngBounds();
             var i, place;
-            for (i = 0; place = places[i]; i++) {
+            for (i=0; place=places[i];i++) {
                 bounds.extend(place.geometry.location);
                 marker.setPosition(place.geometry.location);
             }
