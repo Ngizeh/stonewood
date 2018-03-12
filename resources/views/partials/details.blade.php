d3458682c40ed151d21ca44764d391b919feeacb
Ngizeh <edngize@gmail.com>
2018-03-12 11:32:24 +0300

    all view ports

--- resources/views/partials/details.blade.php
+++ resources/views/partials/details.blade.php
@@ -1,15 +1,12 @@
 <div class="properties-listing">
         <div class="row">
-            <div class="col-lg-3 col-sm-12">
+            <div class="col-lg-3 col-sm-8">
                 @include('partials.search')
-                {{--@if(Auth::check())--}}
-                {{--<a href="{{'/property/create'}}" class="btn btn-primary">Create A Property</a>--}}
-                {{--@endif--}}
             </div>
-            <div class="col-md-9 col-sm-12">
+            <div class="col-lg-9 col-sm-8">
                 <div class="row">
                 @foreach($properties as $property)
-                    <div class="col-md-4 col-sm-12">
+                    <div class="col-md-4 col-sm-6">
                             <div class="properties">
                                 @foreach($property->propertyPhotos as $set)
                                 <div class="image-holder">
